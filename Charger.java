7c704e46fae0c0bdf6285aa78c91112af3421890
jaideephegwad10 <jaideephegwad.xworkz@gmail.com>
2024-07-04 10:48:02 +0530

    java basics code

---
+++ Charger.java
@@ -0,0 +1,8 @@
+class Charger{
+
+	public static void main(String[] phone){
+	
+		System.out.println("A Charger is used to charge a device ");
+		System.out.println("different charges are available for different devices ");
+	}
+}
---
+++ Clothes.java
@@ -0,0 +1,11 @@
+class Clothes{
+
+	public static void main(String[] jai){
+		//display message
+		System.out.println("clothes are basic necessity for human");
+		System.out.println("clothes are of different types based on their purpose");
+		System.out.println("we have different clothes for different occasions");
+		System.out.println("clothes are different in different cultures");
+		System.out.println("we wear variety of clothes based on weather conditions");
+	}
+}
