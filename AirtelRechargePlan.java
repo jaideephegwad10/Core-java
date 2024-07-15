43fa5eef9e53084147730aac50242f8ceed299e1
jaideephegwad10 <jaideephegwad.xworkz@gmail.com>
2024-07-15 22:08:29 +0530

    method overloading java code

--- AirtelRechargePlan.java
+++ AirtelRechargePlan.java
@@ -4,80 +4,74 @@ class AirtelRechargePlan{
 	
 		int planPrice ;
 		
-		if(planName == "50GB data" ){//1
-			planPrice = 361;
-		return planPrice ;
-		}
+		if(planName == "50GB data" )	return planPrice = 361;
 		
-		if(planName == "1GB/day day for 30 days"){//2
-			planPrice = 211;
-		return planPrice ;
-		}
+		if(planName == "1GB/day day for 30 days")	return	planPrice = 211;
 		
-		if(planName == "15GB data"){//3
-			planPrice = 181;
-		return planPrice ;
-		}
+		if(planName == "15GB data")	return	planPrice = 181;
 		
-		if(planName == "unlimited data for 2 days"){//4
-			planPrice = 99;
-		return planPrice ;
-		}
+		if(planName == "unlimited data for 2 days")	return	planPrice = 99;
 		
-		if(planName == "2GB/day for 365 days"){//5
-			planPrice = 3599;
-		return planPrice ;
-		}
+		if(planName == "2GB/day for 365 days")	return	planPrice = 3599;
 		
-		if(planName == "2.5GB/day for 365 days"){//6
-			planPrice = 3999;
-		return planPrice ;
-		}
+		if(planName == "2.5GB/day for 365 days")	return	planPrice = 3999;
 		
-		if(planName == "2.5GB/day for 84 days"){//7
-			planPrice = 1199;
-		return planPrice ;
-		}
+		if(planName == "2.5GB/day for 84 days")	return	planPrice = 1199;
 		
-		if(planName == "3GB/day for 56 days"){//8
-			planPrice = 838;
-		return planPrice ;
-		}
+		if(planName == "3GB/day for 56 days")	return	planPrice = 838;
 		
-		if(planName == "Rs4237.29 talktime"){//9
-			planPrice = 5000;
-		return planPrice ;
-		}
+		if(planName == "Rs4237.29 talktime") return	planPrice = 5000;
+			
+		if(planName == "6GB data for 84 days")	return	planPrice = 509;
+	
+		if(planName == "3GB/day for 28 days")	return	planPrice = 449;
 		
-		if(planName == "6GB data for 84 days"){//10
-			planPrice = 509;
-		return planPrice ;
-		}
+		if(planName == "25GB for 30 days")	return	planPrice = 335;
 		
-		if(planName == "3GB/day for 28 days"){//11
-			planPrice = 449;
-		return planPrice ;
-		}
+		if(planName == "60GB data for 1 month")return	planPrice = 609;
 		
-		if(planName == "25GB for 30 days"){//12
-			planPrice = 335;
-		return planPrice ;
-		}
+		if(planName == "6GB data for 77 days")	return	planPrice = 489;
 		
-		if(planName == "60GB data for 1 month"){//13
-			planPrice = 609;
-		return planPrice ;
-		}
+		if(planName == "847.46 talktime")	return	planPrice = 100;
 		
-		if(planName == "6GB data for 77 days"){//14
-			planPrice = 489;
-		return planPrice ;
-		}
+		else
+			System.out.println(planName+" not found");
+	return 0;	
+	}
+	
+	public static int searchPlans(String planName, int quantity){
+	
+		int planPrice ;
 		
-		if(planName == "847.46 talktime"){//15
-			planPrice = 1000;
-		return planPrice ;
-		}
+		if(planName == "50GB data" )	return planPrice = quantity*361;
+		
+		if(planName == "1GB/day day for 30 days")	return	planPrice = quantity*211;
+		
+		if(planName == "15GB data")	return	planPrice = quantity*181;
+		
+		if(planName == "unlimited data for 2 days")	return	planPrice = quantity*99;
+		
+		if(planName == "2GB/day for 365 days")	return	planPrice = quantity*3599;
+		
+		if(planName == "2.5GB/day for 365 days")	return	planPrice = quantity*3999;
+		
+		if(planName == "2.5GB/day for 84 days")	return	planPrice = quantity*1199;
+		
+		if(planName == "3GB/day for 56 days")	return	planPrice = quantity*838;
+		
+		if(planName == "Rs4237.29 talktime") return	planPrice = quantity*5000;
+			
+		if(planName == "6GB data for 84 days")	return	planPrice = quantity*509;
+	
+		if(planName == "3GB/day for 28 days")	return	planPrice = quantity*449;
+		
+		if(planName == "25GB for 30 days")	return	planPrice = quantity*335;
+		
+		if(planName == "60GB data for 1 month")return	planPrice = quantity*609;
+		
+		if(planName == "6GB data for 77 days")	return	planPrice = quantity*489;
+		
+		if(planName == "847.46 talktime")	return	planPrice = quantity*100;
 		
 		else
 			System.out.println(planName+" not found");
--- AirtelRechargePlanRunner.java
+++ AirtelRechargePlanRunner.java
@@ -5,7 +5,7 @@ class AirtelRechargePlanRunner{
 	public static void main(String[] recharge){
 		System.out.println("main started");
 		
-		int price = AirtelRechargePlan.searchPlans(planName);
+		int price = AirtelRechargePlan.searchPlans(planName, 2);
 		System.out.println("The Price of "+planName+" is "+ price);
 		
 		System.out.println("main started");
